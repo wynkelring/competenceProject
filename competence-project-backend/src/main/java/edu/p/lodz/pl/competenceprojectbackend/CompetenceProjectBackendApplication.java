e7116027afcc0937112c4d61f2b16c1f2a10fbae
Wiktor Nowacki <alterwik@MacBook-Pro-Wiktor.local>
2021-03-13 14:57:23 +0100

    Docker: Create new backend project template on gradle

---
+++ competence-project-backend/src/main/java/edu/p/lodz/pl/competenceprojectbackend/CompetenceProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.p.lodz.pl.competenceprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetenceProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompetenceProjectBackendApplication.class, args);
+    }
+
+}
---
+++ competence-project-backend/src/test/java/edu/p/lodz/pl/competenceprojectbackend/CompetenceProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.p.lodz.pl.competenceprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetenceProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
